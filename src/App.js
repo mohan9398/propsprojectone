2ef5f469672a8480162eb05852874ae5ccd18649
mohan9398 <mohangangadhari49@gmail.com>
2023-12-30 00:48:16 +0530

    propsprojectone

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Card from './components/Card'; 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    
+    <Card moviename="moahn" imsrc="https://www.shutterstock.com/image-vector/online-cinema-art-movie-watching-260nw-586719869.jpg"/>
+    <Card moviename="mouneesh" imsrc="https://w7.pngwing.com/pngs/90/19/png-transparent-film-cinema-movies-miscellaneous-text-logo-thumbnail.png"/>
+ </>
+  )
+
 }
 
 export default App;
